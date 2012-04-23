4d361a4558fd40b28d264c1d0e4f68f2c0c1a443
Sho Kusano <rosylilly@aduca.org>
2012-04-24 05:59:38 +0900

    ざっと必要そうなコンフィグ項目を追加

--- lib/mount_doc/config.rb
+++ lib/mount_doc/config.rb
@@ -1,7 +1,40 @@
 require 'mount_doc'
 
 module MountDoc::Config
-  mattr_accessor :auto_mount
+  mattr_reader :auto_mount
   @@auto_mount = false
-  def self.auto_mount?; !!@@auto_mount; end
+
+  def self.auto_mount=(bool)
+    @@auto_mount = !!bool
+  end
+  def self.auto_mount?; @@auto_mount; end
+
+  mattr_reader :visible_private_methods
+  @@visible_private_methods = false
+
+  def self.visible_private_methods=(visible)
+    @@visible_private_methods = !!visible
+  end
+  def self.visible_private_methods?; @@visible_private_methods; end
+
+  mattr_reader :visible_protected_methods
+  @@visible_protected_methods = false
+
+  def self.visible_protected_methods=(visible)
+    @@visible_protected_methods = !!visible
+  end
+  def self.visible_protected_methods?; @@visible_protected_methods; end
+
+  Components = [
+    :files,
+    :urls,
+    :controllers,
+    :models
+  ]
+  mattr_reader :visible_components
+  @@visible_components = Components
+  def self.visible_components=(components)
+    components.select!{|component| Components.include?(component) }
+    @@visible_components = components
+  end
 end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe MountDoc::Config do
-  its(:auto_mount?){ should be_false }
+  describe :auto_mount do
+    subject{ MountDoc::Config.auto_mount? }
+    it("default: false"){ should be_false }
+
+    it("writable boolean"){
+      MountDoc::Config.auto_mount = 1
+      should be_true
+    }
+  end
+
+  describe :visible_private_methods do
+    subject{ MountDoc::Config.visible_private_methods? }
+
+    it("default: false"){ should be_false }
+
+    it("writable boolean"){
+      MountDoc::Config.visible_private_methods = 1
+      should be_true
+    }
+  end
+
+  describe :visible_protected_methods do
+    subject{ MountDoc::Config.visible_protected_methods? }
+
+    it("default: false"){ should be_false }
+
+    it("writable boolean"){
+      MountDoc::Config.visible_protected_methods = 1
+      should be_true
+    }
+  end
+
+  describe :visible_components do
+    subject{ MountDoc::Config.visible_components }
+
+    it("default: MountDoc::Config::Components"){ should == MountDoc::Config::Components }
+
+    it("writable MountDoc::Config::Components"){
+      MountDoc::Config.visible_components = [:urls, :invalid, :controllers]
+      should == [:urls, :controllers]
+    }
+  end
 end
