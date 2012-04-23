8d131ee2f0363c35922a1083111d82f293f04b62
Sho Kusano <rosylilly@aduca.org>
2012-04-24 04:26:27 +0900

    rails new

---
+++ spec/mock/rails_3.2.3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails323::Application.config.secret_token = 'ff8fa4bf33ee7109fa02acaa4bb69314074889429dc8ed69fc9601a5d09857187a50b78ff4aa6e2e841d04a8e5523f1754aebe9eca933f74ad78a58e644db8ed'
